a65548607f605e9b9b19ce9a2dcc0bcc9f897f94
jinhyejoo <hjjin104@naver.com>
2022-04-15 15:09:15 +0900

    Chapter6 This

---
+++ Chapter6/src/Thisex/CallAnotherConst.java
@@ -0,0 +1,44 @@
+package Thisex;
+
+class Person{
+    String name;
+    int age;
+
+    public Person(){
+        name="No name";
+        age = 0;
+    }
+
+    public Person(String name, int age){
+        this.name =name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+
+public class CallAnotherConst {
+    public static void main(String[] args){
+        Person p1 = new Person();
+        p1.setAge(28);
+        p1.setName("Jin");
+
+        System.out.println(p1.getAge());
+        System.out.println(p1.getName());
+    }
+}
---
+++ Chapter6/src/Thisex/thisEx.java
@@ -0,0 +1,31 @@
+package Thisex;
+
+class birthDay{
+    int day;
+    int month;
+    int year;
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public void printThis(){
+        //this는 자기자신의 인스턴스의 주소값을 print해주기 때문에 사용 가능하다
+        System.out.println(this);
+    }
+}
+
+public class thisEx {
+    public static void main(String[] args){
+        birthDay b1 = new birthDay();
+        birthDay b2 = new birthDay();
+        System.out.println(b1);
+        b1.printThis();
+
+        System.out.println();
+
+        System.out.println(b2);
+        b2.printThis();
+
+    }
+}
