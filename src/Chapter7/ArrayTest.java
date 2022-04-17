6218d6a24bdffb0e02e77c7f2397b64df940373d
jinhyejoo <hjjin104@naver.com>
2022-04-17 21:31:10 +0900

    int&double array

--- src/Chapter7/ArrayTest.java
+++ src/Chapter7/ArrayTest.java
@@ -2,6 +2,19 @@ package Chapter7;
 
 public class ArrayTest {
     public static void main(String[] args) {
-        int[] numbers = new int[] {0,1,2};
+//        int[] numbers = new int[] {0,1,2};
+        int[] numbers = new int[3];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+
+        for(int i=0; i<numbers.length; i++){
+            System.out.println(numbers[i]);
+        }
+
+        double[] studentIDs = new double[5];
+        for(int i=0; i <studentIDs.length;i++){
+            System.out.println(studentIDs[i]);
+        }
     }
 }
