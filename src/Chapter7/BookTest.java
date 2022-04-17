af991d68a9614a591d8290f152b8c95afb5f8056
jinhyejoo <hjjin104@naver.com>
2022-04-17 22:11:27 +0900

    Object Array - Book

---
+++ src/Chapter7/BookTest.java
@@ -0,0 +1,21 @@
+package Chapter7;
+
+public class BookTest {
+    public static void main(String[] args) {
+        Book[] library = new Book[5];
+        for(int i=0; i<library.length; i++) {
+            System.out.println(library[i]);
+        }
+
+        library[0] = new Book("태백산맥", "조정래");
+        library[1] = new Book("토지", "박경리");
+        library[2] = new Book("종의 기원", "정유정");
+        library[3] = new Book("나의 문화유산 답사기", "유홍준");
+        library[4] = new Book("사람은 무엇으로 사는가", "진혜주");
+
+        for(int i=0; i< library.length; i++) {
+            System.out.println(library);
+            library[i].showBookInfo();
+        }
+    }
+}
