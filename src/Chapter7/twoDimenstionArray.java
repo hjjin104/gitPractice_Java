61de00b681ea167f3cec93cb1d6a8312f4cc9231
jinhyejoo <hjjin104@gmail.com>
2022-04-18 11:16:40 +0900

    Chapter 7 twoDimenstionArray

--- src/Chapter7/twoDimenstionArray.java
+++ src/Chapter7/twoDimenstionArray.java
@@ -2,18 +2,18 @@ package Chapter7;
 
 public class twoDimenstionArray {
     public static void main(String[] args) {
-//        int[][] arr = new int[2][3];//{{1, 2, 3}, {4, 5, 6}};
-//        System.out.println(arr.length);  //이중다차원 배열의 행의 길이(개수)
-//        System.out.println(arr[0].length); //첫번째 행의 길이(개수)
-//        System.out.println(arr[1].length);
-//        System.out.println("");
-//
-//        for (int i = 0; i < arr.length; i++) {
-//            for (int j = 0; j < arr[i].length; j++) {
-//                System.out.println(arr[i][j]); //i번째 행의 j번째 열까지의 입력값 출력
-//            }
-////        }
-//
+        int[][] arr = new int[2][3];//{{1, 2, 3}, {4, 5, 6}};
+        System.out.println(arr.length);  //이중다차원 배열의 행의 길이(개수)
+        System.out.println(arr[0].length); //첫번째 행의 길이(개수)
+        System.out.println(arr[1].length);
+        System.out.println("");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.println(arr[i][j]); //i번째 행의 j번째 열까지의 입력값 출력
+            }
+//        }
+
         char[][] alphabets = new char[13][2];
         char ch ='A';
 
